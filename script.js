09270f511ae0cce14ec2460f9c8a01ca9366660d
RohitBiradar <rohitpramilabiradar@gmail.com>
2025-09-07 23:05:16 +0530

    made some changes

--- script.js
+++ script.js
@@ -1,36 +1,104 @@
-// Navbar Links
-const navlinks = document.querySelectorAll(".nav-link");
+gsap.registerPlugin(ScrollTrigger);
 
-function switchSection(targetSection, clickedButton) {
-  navlinks.forEach((btn) => btn.classList.remove("active"));
-  document.getElementById(targetSection).classList.add("active");
-  clickedButton.classList.add("active");
+if (window.innerWidth > 767) {
+  gsap.fromTo(
+    "#name",
+    { x: -200, opacity: 0 },
+    { x: 0, opacity: 1, duration: 1, ease: "power1.inOut" }
+  );
+  gsap.fromTo(
+    "#occupation",
+    { x: 200, opacity: 0 },
+    { x: 0, opacity: 1, duration: 1, ease: "power1.inOut" }
+  );
+  gsap.to(".resume-btn", {
+    rotation: 360,
+    duration: 5,
+    ease: "elastic.out",
+    delay: 1,
+  });
+
+  gsap.to("#about", {
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#about",
+      start: "top top",
+      end: "+=100%",
+      scrub: true,
+      pin: true,
+      pinSpacing: false,
+      anticipatePin: 1,
+    },
+  });
+
+  gsap.fromTo(
+    "#experience",
+    { yPercent: -100, opacity: 0 },
+    {
+      yPercent: 0,
+      opacity: 1,
+      ease: "none",
+      scrollTrigger: {
+        trigger: "#experience",
+        start: "top top",
+        end: "+=100%",
+        scrub: true,
+        pin: true,
+        pinSpacing: false,
+        anticipatePin: 1,
+      },
+    }
+  );
+
+  gsap.timeline({
+    scrollTrigger: {
+      trigger: "#education",
+      start: "top top",
+      end: "+=100%",
+      scrub: 1,
+      pin: true,
+      pinSpacing: false,
+      anticipatePin: 1,
+    },
+  });
+
+  gsap.to(document.querySelector(".skills-grid"), {
+    x: () =>
+      -(document.querySelector(".skills-grid").scrollWidth - window.innerWidth),
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#skills",
+      start: "top top",
+      end: () =>
+        "+=" +
+        (document.querySelector(".skills-grid").scrollWidth -
+          window.innerWidth),
+      scrub: 1,
+      pin: true,
+      anticipatePin: 1,
+    },
+  });
+
+  gsap.to("#projectContainer", {
+    x: () =>
+      -(
+        document.querySelector("#projectContainer").scrollWidth -
+        window.innerWidth
+      ),
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#projects",
+      scrub: 1,
+      pin: true,
+      end: () =>
+        "+=" +
+        (document.querySelector("#projectContainer").scrollWidth -
+          window.innerWidth),
+      anticipatePin: 1,
+    },
+  });
 }
 
-navlinks.forEach((link) => {
-  link.addEventListener("click", (event) => {
-    switchSection(event.currentTarget.dataset.target, event.currentTarget);
-  });
-});
-
-// Mobile Menu Toggle
-const mobileMenuOpen = document.getElementById("mobile-menu-open");
-const mobileMenuClose = document.getElementById("mobile-menu-close");
-const navMenu = document.querySelector("nav");
-
-mobileMenuOpen.addEventListener("click", () => {
-  navMenu.style.display = "block";
-  mobileMenuClose.style.display = "block";
-  mobileMenuOpen.style.display = "none";
-});
-
-mobileMenuClose.addEventListener("click", () => {
-  navMenu.style.display = "none";
-  mobileMenuOpen.style.display = "block";
-  mobileMenuClose.style.display = "none";
-});
-
-// Resume Button
 const resumePDF =
   "https://drive.google.com/file/d/1Gg1jGsF6JcAufNnkdheSdniyaSjJtxfQ/view?usp=sharing";
 
@@ -40,7 +108,6 @@ document.querySelectorAll(".resume-btn").forEach((button) => {
   });
 });
 
-// Project Buttons
 document.querySelectorAll(".project").forEach((button) => {
   button.addEventListener("click", (e) => {
     const buttonType = e.target.textContent.toLowerCase();
